5476f3985415bcd8d6891ca33ed0638ea3c035cd
WILLIAN YOSHIMURA TADA <willian.ytada@senacsp.edu.br>
2018-04-11 11:03:42 -0300

    Initial commit

---
+++ app/src/main/java/br/senac/sp/l13/espacoshoramapp/Area_Usuario_Activity.java
@@ -0,0 +1,13 @@
+package br.senac.sp.l13.espacoshoramapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Area_Usuario_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.area_usuario_layout);
+    }
+}
